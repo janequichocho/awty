18435efed03a8fe316ad6a94ce22ede92809505a
Jane <janequichocho@gmail.com>
2019-05-13 23:15:55 -0700

    Made toast pop up in given timer intervals

--- app/src/main/java/edu/us/ischool/janeq97/arewethereyet/MainActivity.kt
+++ app/src/main/java/edu/us/ischool/janeq97/arewethereyet/MainActivity.kt
@@ -1,16 +1,26 @@
 package edu.us.ischool.janeq97.arewethereyet
 
+import android.app.Activity
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.EditText
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import java.util.*
+import java.util.TimerTask
+
 
 class MainActivity : AppCompatActivity() {
     var messageView: EditText? = null
     var phoneNumView: EditText? = null
     var timeView: EditText? = null
+    var timer: Timer? = null
+    var timerActive = false
+    var handler: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,17 +29,45 @@ class MainActivity : AppCompatActivity() {
         messageView = findViewById(R.id.message)
         phoneNumView = findViewById(R.id.phone_number)
         timeView = findViewById(R.id.time_interval)
+
+        timer = Timer("Timer")
+        handler = Handler()
     }
 
     fun handleStartAndStop(view: View) {
-        var message = messageView?.text.toString()
-        var phoneNumber = phoneNumView?.text.toString()
-        var time = timeView?.text.toString()
+        if (timerActive) {
+            timer?.cancel()
+            timer?.purge()
+            timerActive = false
+        } else {
+            var message = messageView?.text.toString()
+            var phoneNumber = phoneNumView?.text.toString()
+            var time = timeView?.text.toString()
 
-        if (message != "" && phoneNumber != "" && time != "") {
+            if (message != "" && phoneNumber != "" && time != "") {
+                var minutes = time.toInt()
 
 
-            (view as Button).setText("Stop")
+                var timerTask = Task(this, phoneNumber)
+                timerActive = true
+                timer?.schedule(timerTask, (minutes * 60000).toLong(), (minutes * 60000).toLong())
+
+                (view as Button).setText("Stop")
+            }
         }
+    }
+}
+
+
+
+class Task(val context: Context, val phoneNum: String): TimerTask() {
+
+    override fun run() {
+        (context as Activity).runOnUiThread(object: Runnable {
+            override fun run() {
+                Toast.makeText(context, "$phoneNum: Are we there yet?", Toast.LENGTH_LONG).show()
+            }
+        })
+
     }
 }
